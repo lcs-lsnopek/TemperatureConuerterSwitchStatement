1d5fa20e707e73ee8dc1c51ea9a2e98cf42528e9
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-03 08:37:33 -0500

    Initial Commit

---
+++ TemperatureConuerterSwitchStatement/TemperatureConuerterSwitchStatementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConuerterSwitchStatementApp.swift
+//  TemperatureConuerterSwitchStatement
+//
+//  Created by Logan Snopek on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConuerterSwitchStatementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
